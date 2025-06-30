fe6962fa76494a003a2fa64526dd56579e1b5835
Christian Guetnga <lsdrfrx@gmail.com>
2025-06-30 13:28:20 +0300

    chore: add mkDerivation to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,30 @@
-{ pkgs, lib, config, inputs, ... }:
+{ pkgs, lib, ... }:
 
-{
-  packages = with pkgs; [ crystal shards crystalline ];
+let
+  wish = pkgs.stdenv.mkDerivation rec {
+    name = "wish-${version}";
+    version = "0.1.0";
+    src = ./.;
+
+    nativeBuildInputs = with pkgs; [ crystal shards ];
+
+    buildPhase = ''
+      crystal build --release -o wish src/wish.cr
+    '';
+
+    installPhase = ''
+      install -Dm755 wish $out/bin/wish
+    '';
+
+    meta = with pkgs.lib; {
+      description = "Wish is a yet another modern, lightweight shell written in Crystal";
+      homepage = "https://github.com/lsdrfrx/wish";
+      license = licenses.mit;
+      maintainers = [ maintainers."Christian Guetnga" ];
+    };
+  };
+in {
+  packages = [ wish ];
+
+  languages.crystal.enable = true;
 }
