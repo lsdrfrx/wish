e356c81ffd1978550fb9e0f3ceaaeceb588b114f
Christian Guetnga <lsdrfrx@gmail.com>
2025-07-04 11:44:04 +0300

    feat: arrow key hooks
    It's available to move cursor forward/backward and insert/delete characters on cursor
    
    fix: add quote arguments parsing in tokenization
    Now arguments inside quotes does not split by space

--- src/wish/shell.cr
+++ src/wish/shell.cr
@@ -1,14 +1,78 @@
 require "./history.cr"
+require "shellwords"
 
 module Shell
+  @@input_buffer = [] of Char
+  @@cursor_position = 0
+  @@prompt = "#{Dir.current} $ "
+
+  private def self.redraw_line
+    print "\r\e[K#{@@prompt}#{@@input_buffer.join}"
+    print "\e[#{@@prompt.size + @@cursor_position + 1}G"
+  end
+
   def self.read_command : String
-    print "#{Dir.current} $ "
-    gets.to_s.chomp
+    @@input_buffer.clear
+    @@cursor_position = 0
+    self.redraw_line
+
+    loop do
+      char = STDIN.raw &.read_char
+      return "" unless char
+
+      case char
+      when 'q'
+        exit
+      when '\e', '\n'
+        second = STDIN.raw &.read_char
+        third = STDIN.raw &.read_char
+
+        case {second, third}
+        when {'[', 'C'}
+          @@cursor_position += 1 if @@cursor_position < @@input_buffer.size
+          self.redraw_line
+        when {'[', 'D'}
+          @@cursor_position -= 1 if @@cursor_position > 0
+          self.redraw_line
+        when {'[', 'A'}
+          puts "UP"
+        when {'[', 'B'}
+          puts "DOWN"
+        end
+      when '\r'
+        print '\n'
+        break
+      when '\b', 127.chr
+        next if @@cursor_position == 0
+        @@input_buffer.delete_at(@@cursor_position - 1)
+        @@cursor_position -= 1
+        self.redraw_line
+      when '\u{3}'
+        puts "\nEND OF TEXT (CTRL+C)"
+        self.redraw_line
+      when '\u{4}'
+        puts "\nEND OF TRANSMISSION (CTRL+D)"
+        self.redraw_line
+      when '\u{1A}'
+        puts "\nSUBSTITUTE (CTRL+Z)"
+        self.redraw_line
+      else
+        @@input_buffer.insert(@@cursor_position, char)
+        @@cursor_position += 1
+        self.redraw_line
+      end
+    end
+    return @@input_buffer.join
   end
 
   def self.tokenize_command(command : String) : {String, Array(String)}
-    parts = command.split(" ")
-    {parts[0], parts[1..]}
+    return {"", [] of String} if command == ""
+    parts = Shellwords.shellsplit(command)
+
+    command = parts[0]
+    args = parts[1..]
+
+    {command, args}
   end
 
   def self.eval_command(raw_command : String)
@@ -39,7 +103,7 @@ module Shell
     history = History.get_all
     unless history.nil?
       history.each_with_index do |command, index|
-        puts "#{index+1} #{command}"
+        puts "#{index + 1} #{command}"
       end
     end
   end
@@ -61,9 +125,9 @@ module Shell
   private def self.execute_system_command(command : String, args : Array(String), raw_command : String)
     History.save raw_command
     begin
-      Process.run(
+      status = Process.run(
         command,
-        args, 
+        args,
         shell: false,
         output: STDOUT,
         error: STDERR,
@@ -83,7 +147,7 @@ module Shell
     end
   end
 
-  private def self.handle_pwd()
+  private def self.handle_pwd
     History.save "pwd"
     puts Dir.current
   end
